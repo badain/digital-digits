c8139f6bc86a6ba88c2d06febb9f1f6e28b8e13e
badain <rafabadain@gmail.com>
2020-10-20 14:46:34 -0300

    implementa rot_givens()

--- rot-givens.py
+++ rot-givens.py
@@ -6,7 +6,7 @@ import math
 # Dependencies: math
 # Usage: rotation_angle_for_zero(wik, wjk)
 # 
-# Pre-Condition: wik,wjk:int
+# Pre-Condition: wik,wjk:real
 # Post-Condition: returns a dictionary storing sine and
 #                 cossine values that make wjk = 0 when
 #                 applied in a Givens Rotation
@@ -37,10 +37,39 @@ def rotation_angle_for_zero(wik, wjk):
 #        c, s: cossine and sine of rotation angle
 #
 # Pre-Condition: n,m,i,j:int
-#                c,s:float
+#                c,s:real
 #                W(n,m): array length n of lists length m
 #
 # Post-Condition: Returns a W(n,m) matrix Givens Rotated.
 #
 # Author: Rafael Badain @ University of Sao Paulo
 ##########################################################
+def rot_givens(w, n, m, i, j, c, s):
+    # percorre a matrix rotacionando os elementos
+    # das linhas i e j coluna por coluna ate m
+    for r in range(m):
+        aux     = c * w[i][r] - s * w[j][r]
+        w[j][r] = s * w[i][r] + c * w[j][r]
+        w[i][r] = aux
+
+    return w
+
+### debug
+def generate_matrix(n,m):
+    w = [[ 2.,  1.,   1.,  -1.,   1.],
+         [ 0.,  3.,   0.,   1.,   2.],
+         [ 0.,  0.,   2.,   2.,  -1.],
+         [ 0.,  0.,  -1.,   1.,   2.],
+         [ 0.,  0.,   0.,   3.,   1.]]
+#   w = np.zeros((n,m)) # zero matrix
+    return w
+
+w = generate_matrix(5,5)
+print(w)
+angles = rotation_angle_for_zero(w[3-1][3-1], w[4-1][3-1])
+w_givens = rot_givens(w, 5, 5, 3-1, 4-1, angles["c"], angles["s"])
+print(w_givens)
+a = math.sqrt(5)
+print(3 / a)
+print(4 / a)
+print(5 / a)
