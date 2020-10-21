5d3f82ebdc62dce1a6b8c680ea50a4b6d3864da1
badain <rafabadain@gmail.com>
2020-10-21 00:25:33 -0300

    implementa qr_factorization

---
+++ qr_fat.py
@@ -0,0 +1,50 @@
+from rot_givens import rot_givens
+import numpy as np
+
+##########################################################
+# Description: Implements matrix QR Factorization based on
+#              Givens Rotation
+# Dependencies: NumPy, rot_givens
+#
+# Usage: qr_factorization(w,n,m,i,j,k)
+#        W: two-dimensional array
+#        n: # of lines of W
+#        m: # of columns of W
+#        i, j: plane of rotation coordinates
+#        k: column coordinate to be zero
+#        c, s: cossine and sine of rotation angle
+#
+# Pre-Condition: n,m:int
+#                W(n,m): array length n of lists length m
+#
+# Post-Condition: Returns the decomposition of a matrix
+#                 as a upper triangular matrix R
+#
+# Author: Rafael Badain @ University of Sao Paulo
+##########################################################
+
+
+def qr_factorization(w):
+    # obtains matrix size
+    w_len = np.shape(w)
+    n = w_len[0]
+    m = w_len[1]
+    c = 0
+    for k in range(m):
+        for j in range((n-1), k, -1):
+            i = j - 1
+            if w[j][k] != 0:
+                w = rot_givens(w, n, m, i+1, j+1, k+1)
+                c += 1
+        
+    return w
+
+### validation
+w = [[ 2.,  1.,   1.,  -1.,   1.],
+     [ 0.,  3.,   0.,   1.,   2.],
+     [ 0.,  0.,   2.,   2.,  -1.],
+     [ 0.,  0.,  -1.,   1.,   2.],
+     [ 0.,  0.,   0.,   3.,   1.]]
+print(w)
+r = qr_factorization(w)
+print(r)
