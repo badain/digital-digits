1f1219eb8cd03f5db66da7ec3ac9e68e10de0a91
badain <rafabadain@gmail.com>
2020-10-22 13:29:54 -0300

    implementa precisão de representação

--- qr_fat.py
+++ qr_fat.py
@@ -1,9 +1,15 @@
 import argparse
+import sys
 from rot_givens import *
 
 parser = argparse.ArgumentParser(description='Implements matrix QR Factorization based on Givens Rotation.')
 parser.add_argument('--p', '--precision', type=int, nargs='?', const=28, default=28, help='precision of decimal operations', required=False)
+parser.add_argument('--r', '--representation', type=int, nargs='?', const=16, default=16, help='precision of decimal representation', required=False)
 args = parser.parse_args()
+if args.r > args.p:
+    print("usage: qr_fat.py [-h] [--p [P]] [--r [R]](--r <= --p)")
+    print("qr_fat.py: error: argument --r/--representation: --r must be <= --p")
+    sys.exit(1)
 getcontext().prec = args.p
 
 ##########################################################
@@ -37,7 +43,22 @@ def qr_factorization(w):
             i = j - 1                                   # se o elemento é != aplica rot_givens
             if w[j][k] != 0:
                 w = rot_givens(w, n, m, i+1, j+1, k+1)  # desloca os indices para inicio em 1
+
+    if args.p == args.r:
+        return w
+        
+    return matrix_representation_round(w, args.r)
+
+def matrix_representation_round(w, precision):
+    w_len = np.shape(w)
+    n = w_len[0]
+    m = w_len[1]
     
+    precision_str = '1E-' + str(precision)
+    for i in range(n):
+        for j in range(m):
+            w[i][j] = Decimal(w[i][j]).quantize(Decimal(precision_str)).normalize()
+            
     return w
 
 ### validation
