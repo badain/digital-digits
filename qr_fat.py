22a1767fb0d0b683580c85fa03318d9be0aee569
badain <rafabadain@gmail.com>
2020-10-22 14:50:09 -0300

    implementa argumento para printagem opcional

--- qr_fat.py
+++ qr_fat.py
@@ -4,10 +4,11 @@ from rot_givens import *
 
 parser = argparse.ArgumentParser(description='Implements matrix QR Factorization based on Givens Rotation.')
 parser.add_argument('--p', '--precision', type=int, nargs='?', const=28, default=28, help='precision of decimal operations', required=False)
-parser.add_argument('--r', '--rounding', type=int, nargs='?', const=16, default=16, help='precision of result rounding', required=False)
+parser.add_argument('--r', '--rounding', type=int, nargs='?', help='precision of result rounding', required=False)
+parser.add_argument('--e', '--export', default=False, action='store_true', help='prints results',)
 args = parser.parse_args()
-if args.r > args.p:
-    print("usage: qr_fat.py [-h] [--p [P]] [--r [R]](--r <= --p)")
+if args.r is not None and args.r > args.p:
+    print("usage: qr_fat.py [-h] [--p [P]] [--r [R]] [--e]")
     print("qr_fat.py: error: argument --r/--rounding: --r must be <= --p")
     sys.exit(1)
 getcontext().prec = args.p
@@ -17,7 +18,7 @@ getcontext().prec = args.p
 #              Givens Rotation
 # Dependencies: rot_givens, NumPy, math, Decimal
 #
-# Usage: qr_fat.py [-h] [--p [P]] [--r [R]](--r <= --p)
+# Usage: qr_fat.py [-h] [--p [P]] [--r [R]] [--e]
 #
 # optional arguments:
 #  -h, --help            show help message and exit
@@ -26,6 +27,8 @@ getcontext().prec = args.p
 #  --r [R], --rounding [R]
 #                        precision of result rounding
 #                        --r must be <= --p
+#  --e, --export         prints results
+#
 # Pre-Condition: W(n,m): array length n of lists length m
 #
 # Post-Condition: Returns the decomposition of a matrix
@@ -46,10 +49,10 @@ def qr_factorization(w):
             if w[j][k] != 0:
                 w = rot_givens(w, n, m, i+1, j+1, k+1)  # desloca os indices para inicio em 1
 
-    if args.p == args.r:
-        return w
+    if args.r is not None and args.r != args.p:
+        return matrix_representation_round(w, args.r)
         
-    return matrix_representation_round(w, args.r)
+    return w
 
 def matrix_representation_round(w, precision):
     w_len = np.shape(w)
@@ -69,6 +72,5 @@ w = [[ 2.,  1.,   1.,  -1.,   1.],
      [ 0.,  0.,   2.,   2.,  -1.],
      [ 0.,  0.,  -1.,   1.,   2.],
      [ 0.,  0.,   0.,   3.,   1.]]
-print(w)
 r = qr_factorization(w)
-print(r)
+if(args.e): print(r)
