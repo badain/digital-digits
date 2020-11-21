149920cb9382e61aaafbe163825b6c2fb42d9827
badain <rafabadain@gmail.com>
2020-11-21 02:23:20 -0300

    implementa parser, reestrutura sem o main

--- treinamento.py
+++ treinamento.py
@@ -2,8 +2,8 @@
 # Description: treinamento baseado em fatoracao nao negativa
 #              para classificacao de digitos manuscritos utilizando
 #              a database MNIST
-# Dependencies: time
-# Usage: training_MNIST.py ndig_treino
+# Dependencies: time, argparse, fatorização_nao_negativa
+# Usage: training_MNIST.py ndig_treino p --t
 # 
 # Pre-Condition: wik,wjk:real
 # Post-Condition: returns a dictionary storing sine and
@@ -18,36 +18,41 @@ import argparse
 import time
 from fatorização_nao_negativa import*
 
+## Decompoe A = Wd*H por fatoracao nao negativa
+def treino(ndig_treino, p, d):
+    convergencia = [False, 100] # pode mudar
+    a = np.loadtxt("dados_mnist/train_dig" + str(d) + ".txt")
+    decomp = nao_negativa(a[:,:ndig_treino], p, convergencia)
+    return decomp[0] # é a matriz Wd
+
 # Argument Parsing
 parser = argparse.ArgumentParser(description='Decompoe um conjunto de imagens em classificadores, baseado na fatoracao nao negativa')
 parser.add_argument('ndig_treino', type=int, metavar='ndig_treino', help='numero de imagens a ser utilizada para fatoracao')
-#parser.add_argument('--d', '--debug', default=False, action='store_true', help='debug mode',)
+parser.add_argument('p', type=int, metavar='P', help='fator de componentes da decomposicao')
+parser.add_argument('--t', '--times', default=False, action='store_true', help='guarda o tempo de treinamento para cada digito')
 args = parser.parse_args()
 
-## fazer a decomposição Wd*H = A
-def treino(ndig_treino, p, d):
-    convergencia = [False, 100] # pode mudar
-    A = np.loadtxt("dados_mnist/train_dig" + str(d) + ".txt")
-    A = A[:,:ndig_treino]
-    decomp = nao_negativa(A,p,convergencia)
-    return decomp[0] # é a matriz Wd
+# Grava o tempo despendido no treinamento para cada digito
+if(args.t):
+    t = open("train_times.txt", "w")
+    t.write("Tempos para o treinamento de cada digito (em s):\n")
 
-def main():
-    # Dados de entrada
-    ndig_treino = int(input("Número de dígitos para o treinamento: "))
-    p = int(input("Número de componentes (p): "))
-
-    # Treinamento de todos os dígitos
-    t = open("tempos de treinamento.txt", "w")
-    t.write("Tempos para o treinamento de cada dígito (em s):\n")
-    for d in range(9):
+    # Gera a matriz wd para cada digito
+    start_total = time.time()
+    for d in range(10):
         start = time.time()
-        Wd = treino(ndig_treino,p,d)
+        wd = treino(args.ndig_treino, args.p, d)
         elapsed_time = time.time() - start
-        t.write("Dígito " + str(d) + ": " + str(elapsed_time) + "\n")
-        # Output
-        output = "W_" + str(d) + "_" + str(ndig_treino) + "_" + str(p) + ".txt"
-        with open(output, "w") as f:
-            np.savetxt(f, Wd) # problema: muitas casas decimai
+        t.write("d" + str(d) + ": " + str(elapsed_time) + "\n")
+        output = "W_" + str(d) + "_" + str(args.ndig_treino) + "_" + str(args.p) + ".txt"
+        with open(output, "w") as f: np.savetxt(f, wd) # problema: muitas casas decimais
+    elapsed_total = time.time() - start_total
+    t.write("total: " + str(elapsed_total))
     t.close()
-main()
+# Executa o treinamento sem computar o tempo de execucao
+else:
+    # Gera a matriz wd para cada digito
+    for d in range(10):
+        wd = treino(args.ndig_treino, args.p, d)
+        output = "W_" + str(d) + "_" + str(args.ndig_treino) + "_" + str(args.p) + ".txt"
+        with open(output, "w") as f: np.savetxt(f, wd) # problema: muitas casas decimais
